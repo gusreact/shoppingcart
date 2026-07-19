8a18215cc60f4a8d085cdfb67cecc498d9759d67
Villagran Gustavo <gusvillagran@gmail.com>
2026-07-18 21:33:30 -0300

    Was changed the email information on Perfil section.

--- src/components/Perfil/Perfil.tsx
+++ src/components/Perfil/Perfil.tsx
@@ -1,12 +1,12 @@
 import {useAuth} from '../../hooks/useAuth'
 
 const Perfil = () => {
-    const { logout } = useAuth();
+    const { logout, user } = useAuth();
 
     return (
         <>
             <h1>Perfil de Usuario</h1>
-            ¡Hola de nuevo, usuario@email.com!
+            ¡Hola de nuevo, {user?.email}!
             <button onClick={logout}>Cerrar Sesión</button>
         </>
     );
